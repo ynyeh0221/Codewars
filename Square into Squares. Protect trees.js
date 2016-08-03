7fee4fa7ba005f100cc81d49d6503c8c32031af3
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-08-03 15:44:03 -0400

    Update Square into Squares. Protect trees.js

--- Square into Squares. Protect trees.js
+++ Square into Squares. Protect trees.js
@@ -1,29 +1,31 @@
-// Maximum call stack size exceeded when n = 7100
-
 function decompose(n) {
     // your code
     let res = [];
     let flag = 0;
     function dfs(start, path, n, nsquare)
     {
-      let temp = 0;
-      for (let i = 0; i < path.length; i++)
-        temp += Math.pow(path[i], 2);
-      if (temp > nsquare || (temp < nsquare && start == 0))
-        return;
-      if (temp == nsquare)
+      if (nsquare === 0)
       {
         flag = 1;
         res = path;
         return;
       }
+      if (start === 0)
+        return;
       for (let i = 0; i < 2; i ++)
       {
         if (i === 0)
         {
-          let temp = path.slice();
-          temp.push(start)
-          dfs(start - 1, temp, n, nsquare);
+          if (start >= path[path.length-1])
+            return;
+          let new_n = Math.floor(Math.sqrt(nsquare - start * start));
+          let new_nsquare = nsquare - start * start;
+          if (new_n >= 0)
+          {
+            let temp = path.slice();
+            temp.push(start);
+            dfs(new_n, temp, new_n, new_nsquare);
+          }
         }
         else
           return dfs(start - 1, path, n, nsquare);
