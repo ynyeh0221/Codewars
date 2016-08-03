64f126b27a5ef2af4526ccaad2ab3a84294322d4
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-08-03 13:14:50 -0400

    Create Square into Squares. Protect trees.js

---
+++ Square into Squares. Protect trees.js
@@ -0,0 +1,36 @@
+// timeout when n = 7100
+
+function decompose(n) {
+    // your code
+    let res = [];
+    let flag = 0;
+    function dfs(start, path, n, nsquare)
+    {
+      let temp = 0;
+      for (let i = 0; i < path.length; i++)
+        temp += Math.pow(path[i], 2);
+      if (temp > nsquare || (temp < nsquare && start == 0))
+        return;
+      if (temp == nsquare)
+      {
+        flag = 1;
+        res = path;
+        return;
+      }
+      for (let i = 0; i < 2; i ++)
+      {
+        if (i === 0)
+        {
+          let temp = path.slice();
+          temp.push(start)
+          dfs(start - 1, temp, n, nsquare);
+        }
+        else
+          return dfs(start - 1, path, n, nsquare);
+        if (flag == 1)
+          return;
+      }
+    }
+    dfs(n-1, [], n, Math.pow(n, 2));
+    return res.length === 0 ? null : res.reverse();
+}
