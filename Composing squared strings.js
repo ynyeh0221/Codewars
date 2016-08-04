c2217058e854542880b4d98d97794bc027f014fd
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-08-04 14:59:00 -0400

    Create Composing squared strings.js

---
+++ Composing squared strings.js
@@ -0,0 +1,13 @@
+function compose(s1, s2) {
+    // your code
+    s1 = s1.split('\n');
+    s2 = s2.split('\n');
+    let res = "", n = s1.length;
+    for (let i = 0; i < n; i ++)
+    {
+      if (i > 0)
+        res += '\n';
+      res += s1[i].substring(0, i+1) + s2[n-1-i].substring(0, n-i);
+    }
+    return res;
+}
