d137238fd897cdf7252c90d43da9a500ea073bdd
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-08-05 14:13:53 -0400

    Create Playing with digits.js

---
+++ Playing with digits.js
@@ -0,0 +1,11 @@
+function digPow(n, p){
+  // ...
+  let strn = n.toString();
+  let sum = 0;
+  for (let i = 0; i < strn.length; i++)
+  {
+    sum += Math.pow(strn[i], p);
+    p += 1;
+  }
+  return sum % n === 0 ? Math.floor(sum / n) : -1;
+}
