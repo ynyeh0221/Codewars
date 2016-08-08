50566b677c707508d6ee4d955789edb47ae38499
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-08-08 17:25:01 -0400

    Create Directions Reduction.js

---
+++ Directions Reduction.js
@@ -0,0 +1,13 @@
+let comp = {"NORTH": "SOUTH", "WEST": "EAST", "SOUTH": "NORTH", "EAST": "WEST"};
+function dirReduc(arr){
+  // ...
+  let stack = [];
+  for (let i = 0; i < arr.length; i++)
+  {
+    if (stack.length > 0 && comp[arr[i]] == stack[stack.length-1])
+      stack.pop();
+    else
+      stack.push(arr[i]);
+  }
+  return stack;
+}
