a7ffa794e5e2ad8171024138d0a738f3791ca58c
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-08-08 22:24:09 -0400

    Update Bin to Hex and back.py

--- Bin to Hex and back.py
+++ Bin to Hex and back.py
@@ -1,9 +1,6 @@
 letters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']
 
 def bin_to_hex(binary_string):
-    # return the hexadecimal representation of the 
-    # numerical equivalent of binary_string
-    # Good Luck!
     num = 0
     for i in xrange(len(binary_string)):
         if binary_string[len(binary_string)-1-i] == '1':
@@ -16,9 +13,6 @@ def bin_to_hex(binary_string):
     return res[::-1] if res != '' else '0'
     
 def hex_to_bin(hex_string):
-    # return the binary representation of the
-    # numerical equivalent of hex_string
-    # Good Luck!
     hex_string = hex_string.lower()
     num = 0
     for i in xrange(len(hex_string)):
