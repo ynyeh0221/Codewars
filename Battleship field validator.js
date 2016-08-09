2cd5cb7ed0bb6ddf16d9e1cbf76926332c1f67ba
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-08-08 22:57:09 -0400

    Update Battleship field validator.js

--- Battleship field validator.js
+++ Battleship field validator.js
@@ -1,25 +1,24 @@
 function validateBattlefield(field) {
-  // write your magic here
   let ships = {};
-  let len = 0;
-  let direction;
-  let flag = 0;
+  let len = 0; // length of each ship
+  let direction; // dfs direction
+  let invalid = false; // check for dfs direction changing => ship contact
   function dfs(i, j, field, dir)
   {
     if (i < 0 || j < 0 || i >= field.length || j >= field[0].length || field[i][j] != '1')
       return;
     if (dir != direction)
-      flag = 1;
+      invalid = true;
     len += 1;
-    field[i][j] = 2;
+    field[i][j] = 2; // visited
     dfs(i-1, j, field, 'u');
     dfs(i+1, j, field, 'd');
     dfs(i, j-1, field, 'l');
     dfs(i, j+1, field, 'r');
-    dfs(i-1, j-1, field, 'ul');
-    dfs(i-1, j+1, field, 'ur');
-    dfs(i+1, j-1, field, 'dl');
-    dfs(i+1, j+1, field, 'dr');
+    dfs(i-1, j-1, field, 'ul'); // check for contact
+    dfs(i-1, j+1, field, 'ur'); // check for contact
+    dfs(i+1, j-1, field, 'dl'); // check for contact
+    dfs(i+1, j+1, field, 'dr'); // check for contact
   }
   for (let i = 0; i < field.length; i++)
   {
@@ -29,10 +28,10 @@ function validateBattlefield(field) {
       {
         direction = field[i][j+1] == 1 ? 'r' : 'd';
         dfs(i, j, field, direction);
-        if (flag == 1)
+        if (invalid == true)
         {
           return false;
-          flag = 0;
+          invalid = false;
         }
       }
       if (len > 0)
