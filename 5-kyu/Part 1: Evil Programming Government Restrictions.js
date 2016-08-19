b93cc28c58e152fbcb55733b2c1654f88554ba57
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-08-19 17:09:18 -0400

    Create Part 1: Evil Programming Government Restrictions.js

---
+++ 5-kyu/Part 1: Evil Programming Government Restrictions.js
@@ -0,0 +1,3 @@
+var anarchy = function () {
+  return 'I can write numbers like, ' + ('b'.charCodeAt() - 'a'.charCodeAt()).toString() + ', ' + ('c'.charCodeAt() - 'a'.charCodeAt()).toString() + ', ' + ('d'.charCodeAt() - 'a'.charCodeAt()).toString() + '.';
+}
