456682050501d0ae86bd2f26e7e44b338dbb8701
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-08-25 10:54:10 -0400

    Update Simpler Interactive Interpreter.js

--- 2-kyu/Simpler Interactive Interpreter.js
+++ 2-kyu/Simpler Interactive Interpreter.js
@@ -12,7 +12,6 @@ Interpreter.prototype.tokenize = function (program)
 {
     if (program === "")
         return [];
-
     var regex = /\s*([-+*\/\%=\(\)]|[A-Za-z_][A-Za-z0-9_]*|[0-9]*\.?[0-9]+)\s*/g;
     return program.split(regex).filter(function (s) { return !s.match(/^\s*$/); });
 };
