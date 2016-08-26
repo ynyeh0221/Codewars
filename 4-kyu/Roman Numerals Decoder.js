561617472e513845fa3ae6ff059b124a5f615119
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-08-26 10:23:10 -0400

    Update and rename Roman Numerals Decoder>js to Roman Numerals Decoder.js

--- 4-kyu/Roman Numerals Decoder.js
+++ 4-kyu/Roman Numerals Decoder.js
@@ -1,6 +1,4 @@
 function solution(roman){
-  // complete the solution by transforming the 
-  // string roman numeral into an integer 
   var convert = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000};
   var res = convert[roman[0]];
   for (let i = 1; i < roman.length; i++)
