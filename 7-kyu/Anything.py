43dd97550530d26b44d37eacbcd911cb116909d0
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-08-30 19:21:41 -0400

    Create Anything.py

---
+++ 7-kyu/Anything.py
@@ -0,0 +1,20 @@
+class anything(object):
+    def __init__(self, x):
+        pass
+    def __lt__(self, other):
+        return True
+
+    def ___le__(self, other):
+        return True
+
+    def __eq__(self, other):
+        return True
+
+    def __ne__(self, other):
+        return True
+
+    def __gt__(self, other):
+        return True
+
+    def __ge__(self, other):
+        return True
